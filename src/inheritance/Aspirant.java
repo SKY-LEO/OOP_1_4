669ba16f8f9784343679c5904d7bfe2c36a5f4b9
SKY-LEO <leonid311203@gmail.com>
2022-02-23 19:11:25 +0300

    optimisation

--- src/inheritance/Aspirant.java
+++ src/inheritance/Aspirant.java
@@ -3,25 +3,25 @@ package inheritance;
 public class Aspirant extends Student {
     String scientific_work;
 
-    public Aspirant() {
+    Aspirant() {
 
     }
 
-    public Aspirant(String firstName, String lastName, int group, double average_mark, String scientific_work) {
+    Aspirant(String firstName, String lastName, int group, double average_mark, String scientific_work) {
         super(firstName, lastName, group, average_mark);
         this.scientific_work = scientific_work;
     }
 
-    public void setScientific_work(String scientific_work) {
+    void setScientific_work(String scientific_work) {
         this.scientific_work = scientific_work;
     }
 
-    public String getScientific_work() {
+    String getScientific_work() {
         return scientific_work;
     }
 
     @Override
-    public int getScholarship() {
+    int getScholarship() {
         if (this.average_mark == 10) {
             return 200;
         } else {
--- src/inheritance/Student.java
+++ src/inheritance/Student.java
@@ -6,50 +6,50 @@ public class Student {
     int group;
     double average_mark;
 
-    public Student() {
+    Student() {
 
     }
 
-    public Student(String firstName, String lastName, int group, double average_mark) {
+    Student(String firstName, String lastName, int group, double average_mark) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.group = group;
         this.average_mark = average_mark;
     }
 
-    public void setFirstName(String firstName) {
+    void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) {
+    void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public void setGroup(int group) {
+    void setGroup(int group) {
         this.group = group;
     }
 
-    public void setAverage_mark(double average_mark) {
+    void setAverage_mark(double average_mark) {
         this.average_mark = average_mark;
     }
 
-    public String getFirstName() {
+    String getFirstName() {
         return firstName;
     }
 
-    public String getLastName() {
+    String getLastName() {
         return lastName;
     }
 
-    public int getGroup() {
+    int getGroup() {
         return group;
     }
 
-    public double getAverage_mark() {
+    double getAverage_mark() {
         return average_mark;
     }
 
-    public int getScholarship() {
+    int getScholarship() {
         if (this.average_mark == 10) {
             return 100;
         } else {
